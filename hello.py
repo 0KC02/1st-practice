08a8b7767901f46309c07c97bf47643f06e5ed2f
kerry <kerrychun00@gmail.com>
2024-08-07 05:32:40 +0800

    1st try

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("hello world")
+
+name = input("what is your name?")
+print(f"hello , {name}")
+
+num_1 = int(input("give me a number"))
+num_2 = int(input("give me a 2nd number"))
+
+print("The sum of your 2 numbers is:", num_1+num_2)
+print("The difference of your 2 numbers is:", num_1-num_2)
+print("The quotient of your 2 numbers is:", num_1/num_2)
+
+age = int(input("Enter your age: "))
+age_next_year = age+1
+
+print(f"You will be {age_next_year} years old next year")
